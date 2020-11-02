841682f3443ca82584852c904b23b2fdc068c5a3
Taylor SaBell <9723132+taylorsabell@users.noreply.github.com>
2020-11-01 21:16:05 -0700

    Update setup-mac-os.sh

--- setup-mac-os.sh
+++ setup-mac-os.sh
@@ -1,7 +1,14 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+PERSONAL_DEVICE = false
+
 brew cask install google-chrome
 brew cask install visual-studio-code
+
+if [ "$personal-device" = true ]
+then
+  brew cask install 1password-cli
+fi
 
 cd ~/
 echo 'alias g="git"' > .zshrc
