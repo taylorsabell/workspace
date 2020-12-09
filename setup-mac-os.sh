b50ae55e3e2627be2822cd4b48a9ce6f04dcdb21
Taylor SaBell <9723132+taylorsabell@users.noreply.github.com>
2020-12-09 00:14:09 -0700

    Having fun

--- setup-mac-os.sh
+++ setup-mac-os.sh
@@ -1,14 +1,26 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-personal-device=false
+personal=false
+ 
+if [ "$personal" = true ] ; then
+    brew install --cask 1password-cli
+    # op signin example.1password.com email
+fi
+
+rm -fdr ~/.oh-my-zsh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 brew install --cask google-chrome
 brew install --cask visual-studio-code
+brew install jq
 
-if [ "$personal-device" = true ];
-then
-  brew install --cask 1password-cli
+brew install git
+# git config --global user.name "Me"
+
+echo 'alias g="git"' >> ~/.zshrc
+echo 'alias cc="code ."' >> ~/.zshrc
+echo 'alias reload="source ~/.zshrc && echo Reloaded!"' >> ~/.zshrc
+
+if [ "$personal" = true ] ; then
+    echo 'function key() { op get item $1 --fields password | pbcopy && sleep 10 && pbcopy < /dev/null}' >> ~/.zshrc
 fi
-
-cd ~/
-echo 'alias g="git"' > .zshrc
