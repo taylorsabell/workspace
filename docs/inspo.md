126a6fa2a3493a5794076a7bc2f588777986892e
Taylor SaBell <9723132+taylorsabell@users.noreply.github.com>
2020-12-13 15:54:37 -0700

    Added some cool stuff from smart people on the internet

---
+++ docs/inspo.md
@@ -0,0 +1,2 @@
+https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+https://pawelgrzybek.com/change-macos-user-preferences-via-command-line/
--- setup.sh
+++ setup.sh
@@ -21,6 +21,39 @@ brew install git
 # todo - securely automate
 # git config --global user.name "Me"
 # git config --global user.email "email"
+git config --global pull.rebase false
+
+# Preferences
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Show the ~/Library folder
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+
 
 # todo - DRY
 # todo - case-insensitive paths
